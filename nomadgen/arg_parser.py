aae91d3cdc7ecb7d58f4cb72d1ad7c06dd8880bd
uchenic <uchenic@protonmail.com>
2021-04-22 00:51:24 +0300

    add code formatter and fix formatting

--- nomadgen/arg_parser.py
+++ nomadgen/arg_parser.py
@@ -1,14 +1,25 @@
 import argparse
 import pathlib
+
+
 def parse():
-    parser = argparse.ArgumentParser(description='Convert docker-compose file to nomad job')
-    parser.add_argument('compose_file', type=pathlib.Path, help="File to process")
-    parser.add_argument('nomad_job_file', type=argparse.FileType('w', encoding='UTF-8'), help="Output file")
-    parser.add_argument('--registry_base',
-                        help='docker registry base url (for example "registry.access.redhat.com/ubi8/" )')
+    parser = argparse.ArgumentParser(
+        description='Convert docker-compose file to nomad job')
+    parser.add_argument('compose_file',
+                        type=pathlib.Path,
+                        help="File to process")
+    parser.add_argument('nomad_job_file',
+                        type=argparse.FileType('w', encoding='UTF-8'),
+                        help="Output file")
+    parser.add_argument(
+        '--registry_base',
+        help=
+        'docker registry base url (for example "registry.access.redhat.com/ubi8/" )'
+    )
 
     args = parser.parse_args()
     return args
+
 
 if __name__ == '__main__':
     q = parse()
--- nomadgen/compose_defaults.py
+++ nomadgen/compose_defaults.py
@@ -1,19 +1,26 @@
 from collections import ChainMap, defaultdict
 
+
 class ServiceEntry(object):
     """provide some defaults for service entry in docker compose"""
     def __init__(self, compose_service):
         super(ServiceEntry, self).__init__()
         self.compose_service = compose_service
         self.default_service = defaultdict()
-        self.resulting_entry = ChainMap(self.compose_service, self.default_service)
+        self.resulting_entry = ChainMap(self.compose_service,
+                                        self.default_service)
 
     def init(self):
         self.default_service['deploy'] = defaultdict()
-        self.default_service['deploy']['resources'] = {'limits': {'cpus': '0.5', "memory": '256M'}}
+        self.default_service['deploy']['resources'] = {
+            'limits': {
+                'cpus': '0.5',
+                "memory": '256M'
+            }
+        }
 
     def get(self, key, default):
-        return self.resulting_entry.get(key,default)
+        return self.resulting_entry.get(key, default)
 
     def keys(self):
         return self.resulting_entry.keys()
